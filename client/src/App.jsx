73ed99102ce1e438ed6b9e7b623a3083e0d70cde
MLi-Dev <limatthew2006@gmail.com>
2024-08-05 11:11:52 -0700

    Added CORS variable for server side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -185,9 +185,9 @@ const App = () => {
 		requestOptions = {
 			...requestOptions,
 			body: JSON.stringify({
-				username: loginInfo.username,
-				password: loginInfo.password,
-				partyID: loginInfo.partyID,
+				username: loginInfo.username.trim(),
+				password: loginInfo.password.trim(),
+				partyID: loginInfo.partyID.trim(),
 				eidr_id: eidrId,
 			}),
 		};
