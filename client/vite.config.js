c0728d211fd6eea320bb7ce03c81d375f80253b3
MLi-Dev <limatthew2006@gmail.com>
2024-08-28 18:17:46 -0700

    Changed deploy script, rearranged structure of build directory.

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+	base: "/generatebmr/",
 	plugins: [react()],
+	build: {
+		outDir: "build", // Output directory for the build
+	},
 	server: {
 		proxy: {
 			"/resolve": {
